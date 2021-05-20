3b01dccce431eb2a543d3c5767a92a172f575b72
yutingy <yutingy@github.com>
2021-05-20 11:56:52 -0400

    more cleanup

--- src/EntryData.java
+++ src/EntryData.java
@@ -26,37 +26,6 @@ public class EntryData {
     }
 
 
-    public boolean compareWithChange(List<Condition> graphChangeConditions, GraphChange graphChange){
-        //Method used to compare this EntryData object (a graph change) with other EntryData objects (view!); each object contains a condition set and
-        //depending on the type of graph change, we check differently
-
-        //returns true if the graph change affects the EntryData given as input
-
-        switch(graphChange.getGraphChangeType()){
-
-            case DELETE:{
-
-
-            }
-
-            case INSERT:{
-
-            }
-
-            case UPDATE:{
-
-            }
-
-
-        }
-
-
-
-
-
-        return false;
-    }
-
     public void setDependents(List<String> dependents) {
         this.dependents = dependents;
     }
